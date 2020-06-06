a8f0024cf86eea22968c222113c812358d6dc10e
fancy164 <66501889+fancy164@users.noreply.github.com>
2020-06-06 09:43:40 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+
+  console.log(`Logged in as ${client.user.tag}!`);
+
+});
+
+
+
+client.on('message', msg => {
+
+  if (msg.content === 'hai acchan') {
+
+    msg.reply('Hai... apa kabar? Semoga betah di Nippon Teikoku ya :innocent:');
+
+  }
+
+  if (msg.content === 'konnichiwa') {
+      
+    msg.reply('Konnichiwa, kyou wa ii tenki desu ne :grin:');
+
+  }
+
+  if (msg.content === 'ohayou'){
+
+    msg.reply('ohayou gozaimasu, waduh... masih pagi udah main Discord aja :rofl:');
+
+  }
+
+  if (msg.content === 'fuck'){
+
+    msg.reply('aduh... anak kecil gak boleh ngomong gitu :rage:');
+    
+  }
+
+  if (msg.content === 'Mari mengenang Kaori'){
+
+    msg.reply('https://www.youtube.com/watch?v=e7q38DZPE9I');
+    
+  }
+  
+});
+
+
+
+client.login('NzE2MDkyMjYwMzY1NjMxNTIw.Xtr0PA.DXVD5dHQZaCMn-PRil_HvtT05mk');
